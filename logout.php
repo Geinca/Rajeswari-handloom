6c3d2643acfd22f9529dab5b76bdbec2c7ddb4b2
jayashree patra <jayashreepatra2001@gmail.com>
2025-05-30 11:17:09 +0530

    logout code modify

--- logout.php
+++ logout.php
@@ -1,11 +1,24 @@
 <?php
 session_start();
 
-// Clear all session data
-$_SESSION = [];
+// Unset all session variables
+$_SESSION = array();
+
+// If session uses cookies, delete the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 
 // Destroy the session
 session_destroy();
+
+// Optionally add a logout message
+session_start();
+$_SESSION['message'] = "You have been logged out successfully.";
 
 // Redirect to login page
 header("Location: index.php");
